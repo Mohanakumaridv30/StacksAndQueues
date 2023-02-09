1e24756c7d069c749c98205b4da37f63963bad4c
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-02-10 00:12:45 +0530

    Added : Ability to peak and

--- src/com/bridgelabz/stacksandqueues/Stack.java
+++ src/com/bridgelabz/stacksandqueues/Stack.java
@@ -1,19 +1,47 @@
 package com.bridgelabz.stacksandqueues;
 
 public class Stack <T> {
-      //creating Object
 
+    /*
+     * creating Object
+     */
     LinkedList linkedList = new LinkedList();
 
-    // pushing the value at first place calling method through object
-
+    /*
+     * pushing the value at first place calling method through object
+     */
     public void push(Object data) {
         linkedList.push(data);
     }
 
-    // calling method to display value in linkedList/stack
-
+    /*
+     * calling method to display value in linkedList/stack
+     */
     public void displayStack() {
         linkedList.show();
+    }
+
+    public void peek() {
+        if (linkedList.head != null) {
+            System.out.println("Peaked from stack : " + linkedList.head.key);
+        } else {
+            System.out.println("No element left in the stack.");
+        }
+    }
+
+    /*
+     * 5.method to delete peaked value
+     */
+    public Node pop() {
+        peek();
+        linkedList.head = linkedList.head.next;
+        return linkedList.head;
+    }
+
+    public void emptyStack() {
+        while (linkedList.head != null) {
+            pop();
+        }
+        peek();
     }
 }
